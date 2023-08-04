324923aa9c28cbdddf8486988f982b0060c8a696
Aman Singh <amansinghs7010@gmail.com>
2023-08-04 17:40:53 -0400

    remove dependency

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 import { Order, OrderStatus } from './order';
 
@@ -53,7 +52,17 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.set('versionKey', 'version');
-ticketSchema.plugin(updateIfCurrentPlugin);
+
+// function keyword is required
+// Optimistic concurrency control
+// prevent previous versions of a document from being saved over the current version.
+ticketSchema.pre('save', function (done) {
+  this.$where = {
+    version: this.get('version') - 1,
+  };
+
+  done();
+});
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
