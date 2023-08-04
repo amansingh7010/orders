df84bc56e4f322c2ea864fc300d5be591ccfec0c
Aman Singh <amansinghs7010@gmail.com>
2023-08-04 15:12:50 -0400

    created event listeners

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -5,6 +5,7 @@ import { Order, OrderStatus } from './order';
 // Interface that describes the properties
 // required to create a new Ticket
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -46,7 +47,11 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  });
 };
 
 // function keyword is required
